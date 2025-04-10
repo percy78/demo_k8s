c2d4939b4cf70e9e77b9c622d4af0baf395f50c8
percy78 <ola.gustafssons@teliacompany.com>
2025-04-10 11:20:23 +0200

    added more values

--- rust_learning/variables/src/main.rs
+++ rust_learning/variables/src/main.rs
@@ -21,6 +21,10 @@ fn main() {
         let _seven_point_three = q.1;
         let _nine = q.2;
 	let h = [1, 2, 3, 4, 5];
+	let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+	
+
 
 	{
 	    let x = x * 2;
@@ -42,6 +46,13 @@ fn main() {
 	println!("The value of b is: {_b}");
 	println!("The value of q is: {_seven_point_three}");
 	println!("The value of h is: {h:?}");
+	println!("The value of months is: {months:?}");
+	println!("The value of months[0] is: {}", months[0]);
+	println!("The value of months[1] is: {}", months[1]);
+	println!("The value of months[2] is: {}", months[2]);
+	println!("The value of months[3] is: {}", months[3]);
+	println!("The value of months[4] is: {}", months[4]);
+	println!("The value of months[5] is: {}", months[5]);			
 
 	    
     }
