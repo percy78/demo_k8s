8974723269d11c04c5e56456336d1dcb8ad54956
percy78 <percy78@gmail.com>
2025-08-05 13:59:33 +0200

    added functions

--- rust_learning/functions/src/main.rs
+++ rust_learning/functions/src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
     println!("Hello, world!");
 
-        another_function();
+    another_function(5);
+    and_another_function();
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 6 / 3 - 2 * 8
+    };
+
+    println!("The value of y is: {y}");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("Another function. {}", x);
+}
+
+fn and_another_function() {
+    println!("And another function.");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+    let b = five();
+
+println!("The value of b is: {b}");
 }
